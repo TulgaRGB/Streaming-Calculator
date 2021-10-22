258939ae9a2439db4f654cb46f24ab399249ace9
Filippo Piggici <filippo.piggici@treatwell.com>
2021-10-22 12:02:04 +0100

    update support button

--- src/components/Support/Support.tsx
+++ src/components/Support/Support.tsx
@@ -4,7 +4,7 @@ import styles from "./Support.module.css";
 export function Support(): React.ReactElement {
   return (
     <p className={styles.supportLink}>
-      Support this website by listening to my music here -&gt;
+      Support this website by listening to my music here
       <div className={styles.button}>
         <a href={"https://bio.to/moyo"} aria-label={"My music"}>
           Moyo
