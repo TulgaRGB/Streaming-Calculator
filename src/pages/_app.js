d6eb50bce02b34014943120760fcd1e4cec92bb0
Filippo Piggici <filippo.piggici@treatwell.com>
2021-11-04 12:20:03 +0000

    remove zoom on safari ios

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -26,6 +26,7 @@ export default function MyApp({ Component, pageProps }) {
           crossOrigin="anonymous"
         />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <title>
           Estimate your earnings (2021 update) | Music Streaming Calculator
         </title>
