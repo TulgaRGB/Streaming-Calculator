5213d70ba166a15a5437992e4a99259f6d0afa3e
Filippo Piggici <filippo.piggici@treatwell.com>
2021-11-15 16:08:34 +0000

    remove additional spaces

--- src/components/Support/Support.tsx
+++ src/components/Support/Support.tsx
@@ -7,17 +7,17 @@ export function Support(): React.ReactElement {
   return (
     <div className={styles.supportLink}>
       <div className={styles.flex}>
-        <FiInstagram size={"16px"} /> Follow this page on &#160;
+        <FiInstagram size={"16px"} /> Follow this page on&#160;
         <div className={styles.button}>
           <a href={"https://www.instagram.com/streamingcalculator/"}>
             Instagram
           </a>
         </div>
-        &#160; for the latest updates, tips and roadmap.
+        &#160;for the latest updates, tips and roadmap.
       </div>
       <div className={styles.flex}>
         <CgMusicNote size={"16px"} />
-        Support this website by listening to &#160;
+        Support this website by listening to&#160;
         <div className={styles.button}>
           <a href={"https://bio.to/moyo"}>my music</a>
         </div>
