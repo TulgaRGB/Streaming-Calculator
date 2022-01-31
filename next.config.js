e031083221ee29ca4ca57201ffac4a339088a2b7
Filippo <filippo@outverse.com>
2022-01-31 09:56:56 +0000

    move to preact, reduce bundle size by 30kb

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  webpack(config, { dev, isServer }) {
+    // ${previousConfig...}
+
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+
+    return config;
+  },
+};
